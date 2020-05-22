3d2639d8f632c48d181de632ea422bc5197cf7e9
Jahteo <65362632+Jahteo@users.noreply.github.com>
2020-05-22 11:17:17 -0700

    Add files via upload
    First commit

---
+++ VS Code website files 5.20.2020/index.js
@@ -0,0 +1,34 @@
+// define the function to operate your button here 
+
+
+/*eventually: create dropdown & button to double & 4x the recipe ingredients.
+use an array or object to contain the diff numbers
+create array one, then use array[ingredient]*2 (but properly coded)
+if (serving=original){ingredientScale=ingredientsOriginal};
+else if (servings = doubled){ingredientScale = ingredientsDoubled};
+esle if (servings = quintupled){ingredientScale=ingredientsQuintupled};*/
+
+
+function showOrHide(){
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var readMoreButtonText = document.getElementById("readMoreButton");
+
+    if (dots.style.display==="none"){
+        dots.style.display="inline";
+        moreText.style.display="none";
+        readMoreButtonText.innerHTML="Read Less";
+        //this might be related to a way I can display diff ingredient numbers!!!!
+    } 
+    else{
+        dots.style.display="none";
+        moreText.style.display="inline";
+        readMoreButtonText.innerHTML="Read More";
+    }
+}
+
+
+function postComment() = {
+    console.log(textarea);
+    //????? update to "form" vs button later
+}
